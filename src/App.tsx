47a419626cf3e7a79832b9b35f31af29bfccd624
Reza Aditama <aditamareza03@gmail.com>
2025-07-08 14:47:42 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className='min-h-screen flex justify-center items-center bg-blue-900'>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+export default App;
